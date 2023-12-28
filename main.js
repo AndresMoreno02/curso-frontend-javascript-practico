1eaa318ead64861370aa3a52bb32d9e390ad4f9f
Andres Moreno <felian11@hotmail.com>
2023-12-28 20:12:39 -0300

    Agrego a index el menu categorias solo para mobile, and interaccion desde

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', togglemobileMenu)
 
 function toggleDesktopMenu () {
     desktopmenu.classList.toggle('inactive');
 }
 
-console.log("Holas")
+function togglemobileMenu () {
+    mobileMenu.classList.toggle('inactive');
+}
