927f469c546324696be941f088b94d9d01672387
Andres Moreno <felian11@hotmail.com>
2023-12-29 23:25:59 -0300

    Agregar el carrito de compras, con JS

--- main.js
+++ main.js
@@ -2,15 +2,42 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-
-menuEmail.addEventListener('click', toggleDesktopMenu)
-menuHamIcon.addEventListener('click', togglemobileMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', togglemobileMenu);
+menuCarrito.addEventListener('click', toggleMenuCarrito);
 
 function toggleDesktopMenu () {
+
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if(!isAsideClose) {
+        aside.classList.add('inactive')
+    }
+
     desktopmenu.classList.toggle('inactive');
 }
 
 function togglemobileMenu () {
+
+    const isAsideClose = aside.classList.contains('inactive');
+
+    if(!isAsideClose) {
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMenuCarrito () {
+   const iMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if(!iMobileMenuClose) {
+        mobileMenu.classList.add('inactive')
+    }
+
+   aside.classList.toggle('inactive')
+
 }
